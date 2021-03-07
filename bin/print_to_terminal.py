046030cd8937401f936fd0d06f0e68e5ffc42a09
Anton de Winter <adewinter@gmail.com>
2021-03-07 14:38:04 -0800

    Added ability to specify serial port so that it will work on other operating systems

--- bin/print_to_terminal.py
+++ bin/print_to_terminal.py
@@ -3,12 +3,17 @@ import serial
 import signal
 import sys
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Dump the values read by the teensy via serial port out to this terminal.')
+parser.add_argument('serial_port_name', metavar='SERIAL_PORT', help="The name of the serial port to connect to. E.g. 'COM1' (windows) or '/dev/tty.usbserial-1' (OSX). See this link to find the serial port on macs: https://stackoverflow.com/questions/12254378/how-to-find-the-serial-port-number-on-mac-os-x")
+
 
 serial_port = serial.Serial()
 
-def connect():
+def connect(serial_port_name):
 	serial_port.baudrate = 9600
-	serial_port.port = 'COM7'
+	serial_port.port = serial_port_name
 	serial_port.open()
 	print("connected to: " + serial_port.portstr)
 
@@ -30,6 +35,11 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 if __name__ == '__main__':
-	connect()
+	if len(sys.argv) < 2:
+	    parser.print_help()
+	    print('')
+	args = parser.parse_args()
+	connect(args.serial_port_name)
 	time.sleep(1)
 	read_serial_data_and_print()
+
